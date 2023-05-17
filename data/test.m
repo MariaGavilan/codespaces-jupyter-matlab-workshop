3c38081d7664082dd8ef22987abbb480a298da9e
Prabhakar Kumar <prabhakk@mathworks.com>
2023-05-18 00:21:58 +0530

    push sample M file

---
+++ data/test.m
@@ -0,0 +1,12 @@
+function y = test(x)
+    for i = 1:10
+        y = x + 1;
+    end
+    switch y
+        case 1:
+            case 2:
+                case 3:
+            
+        otherwise
+            
+    end
