76348d7b912432643a2e807aeaec114ae8db52eb
Mike Croucher <mikecroucher@users.noreply.github.com>
2023-06-21 22:12:35 +0000

    Pending changes exported from your codespace

--- notebooks/Exercise3/weather.py
+++ notebooks/Exercise3/weather.py
@@ -104,3 +104,10 @@ def parse_forecast_json(json_data):
         
     except:
         print('Something went wrong while parsing forecast json')
+
+if __name__=="__main__":
+    print("Running weather.py as a script")
+    print("Getting current weather for London")
+    appid ='b1b15e88fa797225412429c1c50c122a1';
+    jsonData = get_current_weather("London","UK",appid,api='samples')
+    print(jsonData)
