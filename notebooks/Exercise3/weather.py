7338a721415fd67c3c9f931e63985f843b3b9d31
Maria Elena Gavilan <50922741+MariaGavilan@users.noreply.github.com>
2023-06-21 14:56:22 -0400

    Updating files to recent version

--- notebooks/Exercise3/weather.py
+++ notebooks/Exercise3/weather.py
@@ -2,55 +2,41 @@
 '''
 Created on Thu Jan  4 20:41:45 2018
 Modified Jan 29 2019
-
-@author: hgorr
+Simplified June 16 2022
 
 '''
 
 # weather.py
-import csv
 import datetime
 import json  
 import urllib.request
 
-BASE_URL = 'https://api.openweathermap.org/data/2.5/{}?q={},{}&units={}&appid={}'
-FORECAST_KEYS = {'current_time':'DateLocal', 'temp':'T', 'deg':'WindDir',
-                 'speed':'WindSpd', 'humidity':'RH', 'pressure':'P'}
-
-def read_backup(city):
-    '''Read example data from a backup file'''
-
-    with open('backupdata.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for s in [*reader]:
-            if s['city'] == city:
-                data = dict(s)
-                data['City'] = city # Used for error checking below
-                # Convert data types
-                for k in ('temp', 'pressure', 'humidity', 'temp_min', 
-                          'temp_max', 'speed', 'deg', 'lat', 'lon'):
-                    data[k] = float(data[k])
-                return data
-    return None
-
+BASE_URL = '/data/2.5/{}?q={},{}&appid={}'
 
 def get_current_weather(city, country, apikey,**kwargs):  
     '''get current conditions in specified location
-    get_current_weather('boston','us',key,units='metric')'''
-            
-    info = {'units':'imperial'}     
-    for key, value in kwargs.items():
-        info[key] = value
+    appid='b1b15e88fa797225412429c1c50c122a1'
+    get_current_weather('London','uk',appid,api='samples')'''
+
+    # select between samples or prod api
+    if 'api' in kwargs:
+        if kwargs['api'] == 'samples':
+            DOMAIN = "http://samples.openweathermap.org"
+        else:
+            print('not the right api arg')
+            DOMAIN = "http://api.openweathermap.org"
+    else:
+        DOMAIN = "http://api.openweathermap.org"
+
     # Read current conditions
     try:
-        # url = 'https://api.openweathermap.org/data/2.5/weather?q=Boston,us&appid=11111'
-        url = BASE_URL.format('weather',city,country,info['units'],apikey)
-        json_data = json.loads(urllib.request.urlopen(url).read())        
+        # url = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1'
+        url = DOMAIN + BASE_URL.format('weather',city,country,apikey)
+        json_data = json.loads(urllib.request.urlopen(url).read())  
+        return json_data 
     except urllib.error.URLError:
-        # If weather API doesnt work, read the file
-        json_data = read_backup(city)          
-
-    return json_data
+        # If weather API doesnt work
+        print("API not available")
 
 
 def parse_current_json(json_data):
@@ -79,28 +65,27 @@ def parse_current_json(json_data):
 
 
 def get_forecast(city, country, apikey, **kwargs):
-    '''get forecast conditions in specified location'''
-        
-    # include keyword args for numdays=3  and units='metric'    
-    info = {'units':'imperial', 'days':5}     
-    for key, value in kwargs.items():
-        info[key] = value
-        
+    '''get forecast conditions in specified location
+    appid='b1b15e88fa797225412429c1c50c122a1'
+    get_forecast('Muenchen','DE',appid,api='samples')'''
+
+    # select between samples or prod api
+    if 'api' in kwargs:
+        if kwargs['api'] == 'samples':
+            DOMAIN = "http://samples.openweathermap.org"
+        else:
+            print('not the right api arg')
+            DOMAIN = "http://api.openweathermap.org"
+    else:
+        DOMAIN = "http://api.openweathermap.org"
     # get forecast    
     try:
-        url = BASE_URL.format('forecast',city,country,info['units'],apikey)
-        json_data = json.loads(urllib.request.urlopen(url).read())    
+        url = DOMAIN + BASE_URL.format('forecast',city,country,apikey)
+        json_data = json.loads(urllib.request.urlopen(url).read())   
+        return json_data 
     except: 
-        with open('backupforecast.csv', newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            json_data = {'City':city}
-            for key in FORECAST_KEYS.keys():
-                json_data[key] = []
-            for s in [*reader]:
-                if s['City'] == city:
-                    for key,value in FORECAST_KEYS.items():
-                        json_data[key].append(dict(s)[value])
-    return json_data
+        print("API not available")
+
 
 def parse_forecast_json(json_data):
     '''parse and extract json data from the weather forecast data'''     
@@ -109,20 +94,13 @@ def parse_forecast_json(json_data):
         # parse forecast json data
         data = json_data['list']
         wind_keys = ['deg','speed']
-        weather_info = dict(zip(FORECAST_KEYS.keys(), 
-                                [[] for i in range(len(FORECAST_KEYS))]))
+        weather_info = {'current_time':[], 'temp':[], 'deg':[],
+                        'speed':[], 'humidity':[], 'pressure':[]}
         for data_point in data[0:40]:
-            for k in list(FORECAST_KEYS.keys())[1:]: #Taking a slice so we don't add the city every time
+            for k in list(weather_info.keys())[1:]: #Taking a slice so we don't add the time
                 weather_info[k].append(float(data_point['wind' if k in wind_keys else 'main'][k]))
             weather_info['current_time'].append(data_point['dt_txt'])
-    except KeyError as e:
-        # use current dictionary (because it probably came from backup file) 
-        try:
-            # If this fails then the json_data didn't come from backup file
-            json_data.pop('City') 
-            weather_info = json_data
-        except:
-            # print('Something else went wrong while parsing forecast json')
-            raise e
-
-    return weather_info
+        return weather_info
+        
+    except:
+        print('Something went wrong while parsing forecast json')
